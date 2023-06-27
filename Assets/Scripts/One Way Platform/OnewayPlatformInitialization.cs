dc150abca6657c94eae8f3297765d78a0bef52a4
Maxime <enzu.mc@gmail.com>
2023-06-27 08:46:25 +0200

    one way platform 2

--- Assets/Scripts/One Way Platform/OnewayPlatformInitialization.cs
+++ Assets/Scripts/One Way Platform/OnewayPlatformInitialization.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class OnewayPlatformInitialization : MonoBehaviour
 {
+    public Sprite[] platformTiles;
     [SerializeField] private GameObject tile;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
+    public bool wallOnTheLeft;
+    public bool wallOnTheRight;
 
     void Start()
     {
@@ -15,21 +18,33 @@ public class OnewayPlatformInitialization : MonoBehaviour
 
         coll.enabled = false; //Disable collision during initialization
 
-        //Resize collision with sprite size
-        BoxCollider2D newColl = coll;
-        newColl.size = new Vector2(sprite.size.x, newColl.size.y);
-
         ///Left edge
         GameObject leftTile = Instantiate(tile, transform.position, Quaternion.identity);
         leftTile.transform.SetParent(transform);
-        leftTile.transform.position = new Vector2(leftTile.transform.position.x - (sprite.size.x - 1), leftTile.transform.position.y);
-        leftTile.GetComponent<OnewayTile>().state = -1;
+        leftTile.transform.position = new Vector2(leftTile.transform.position.x - (2 * sprite.size.x - 1) / 4, leftTile.transform.position.y);
+
+        if (wallOnTheLeft)
+        {
+            leftTile.GetComponent<SpriteRenderer>().sprite = platformTiles[0];
+        }
+        else
+        {
+            leftTile.GetComponent<SpriteRenderer>().sprite = platformTiles[1];
+        }
 
         ///Right edge
         GameObject rightTile = Instantiate(tile, transform.position, Quaternion.identity);
         rightTile.transform.SetParent(transform);
-        rightTile.transform.position = new Vector2(rightTile.transform.position.x + (sprite.size.x - 1), rightTile.transform.position.y);
-        rightTile.GetComponent<OnewayTile>().state = 1;
+        rightTile.transform.position = new Vector2(rightTile.transform.position.x + (2 * sprite.size.x - 1) / 4, rightTile.transform.position.y);
+
+        if (wallOnTheRight)
+        {
+            rightTile.GetComponent<SpriteRenderer>().sprite = platformTiles[4];
+        }
+        else
+        {
+            rightTile.GetComponent<SpriteRenderer>().sprite = platformTiles[3];
+        }
 
         //Create middle tile (if sprite size >= 1.5)
         if (sprite.size.x >= 1.5)
@@ -37,11 +52,13 @@ public class OnewayPlatformInitialization : MonoBehaviour
             GameObject middleTile = Instantiate(tile, transform.position, Quaternion.identity);
             middleTile.transform.SetParent(transform);
             middleTile.transform.position = transform.position;
-            middleTile.GetComponent<OnewayTile>().state = 0;
+            middleTile.GetComponent<SpriteRenderer>().sprite = platformTiles[2];
             middleTile.GetComponent<SpriteRenderer>().size = new Vector2(sprite.size.x - 1, sprite.size.y);
         }
 
         sprite.enabled = false;
         coll.enabled = true;
+
+        Debug.Log(coll.bounds.size.y);
     }
 }
---
+++ Assets/Scripts/VerticalOneWayCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalOneWayCollision : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private BoxCollider2D coll;
+    private BoxCollider2D playerColl;
+
+    private void Start()
+    {
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        playerColl = player.GetComponent<BoxCollider2D>();
+
+        if (playerColl.bounds.center.y - (coll.bounds.center.y + coll.bounds.size.y / 2 + playerColl.bounds.size.y / 2) > 0f)
+        {
+            coll.enabled = true;
+        }
+        else
+        {
+            coll.enabled = false;
+        }
+
+        Debug.Log(playerColl.bounds.center.y);
+    }
+}
